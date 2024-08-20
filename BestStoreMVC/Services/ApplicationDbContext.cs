736f926f1ad23b675e4e15aacfb350f6202c76b9
keneikevin <40203432+keneikevin@users.noreply.github.com>
2024-08-20 21:25:25 +0300

    Add project files.

---
+++ BestStoreMVC/Services/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿
+using BestStoreMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options): base(options)    
+    {
+        
+    }
+    public DbSet<Product> Products { get; set; }
+}
